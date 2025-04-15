7932477868c1f7a30c97435eaf0cc920b9bc63f2
xxKuzi <jakub.kuzel7@gmail.com>
2025-04-16 00:32:14 +0200

    Home upgrade

--- src/components/CalibrationMenu.jsx
+++ src/components/CalibrationMenu.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { useData } from "@/parts/Memory";
 
 export default function CalibrationMenu() {
-  return <div>CalibrationMenu</div>;
+  const { lastCalibration } = useData();
+  return (
+    <div className="border-2 border-sky-400 hover:border-gray-300 duration-300 w-[492px] px-4 py-1 rounded-lg mt-8">
+      <h2 className="text-4xl">Kalibrace</h2>
+      <p>Poslední kalibrace byla provedena: {lastCalibration}</p>
+    </div>
+  );
 }
