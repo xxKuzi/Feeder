737d02e3f25ac5c2cfc7b4d3cecd58aac7b8888a
xxKuzi <jakub.kuzel7@gmail.com>
2025-04-22 13:18:00 +0200

    everyday calibration, cal button to calibration widget to home

--- src/components/CalibrationMenu.jsx
+++ src/components/CalibrationMenu.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { useData } from "@/parts/Memory";
 
 export default function CalibrationMenu() {
-  const { lastCalibration } = useData();
+  const { lastCalibration, openCalibration } = useData();
   return (
-    <div className="border-2 border-sky-400 hover:border-gray-300 duration-300 w-[492px] px-4 py-1 rounded-lg mt-8">
+    <div className="relative border-2 border-sky-400 hover:border-gray-300 duration-300 w-[492px] px-4 py-1 rounded-lg mt-8">
       <h2 className="text-4xl">Kalibrace</h2>
       <p>Poslední kalibrace byla provedena: {lastCalibration}</p>
+      <button
+        onClick={() => openCalibration()}
+        className="absolute top-2 right-2 button__small button__positive"
+      >
+        kalibrační menu
+      </button>
     </div>
   );
 }
