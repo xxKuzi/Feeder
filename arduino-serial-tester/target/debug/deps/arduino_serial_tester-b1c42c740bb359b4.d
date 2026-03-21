d65f9ea851a332fcb11f0df0a2669c1f9ac544b4
Jakub Kužel <jakub.kuzel7@gmail.com>
2026-03-21 12:00:14 +0100

    added arduino-serial-tester to gitignore

---
+++ arduino-serial-tester/target/debug/deps/arduino_serial_tester-b1c42c740bb359b4.d
@@ -0,0 +1,5 @@
+/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/deps/libarduino_serial_tester-b1c42c740bb359b4.rmeta: src/main.rs
+
+/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/deps/arduino_serial_tester-b1c42c740bb359b4.d: src/main.rs
+
+src/main.rs:
