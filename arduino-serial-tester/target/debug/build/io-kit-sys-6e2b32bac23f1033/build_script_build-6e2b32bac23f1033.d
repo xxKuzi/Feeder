56d50e8da0e0a95b023f1bf2e9c9864399709ef7
Jakub Kužel <jakub.kuzel7@gmail.com>
2026-03-21 12:20:04 +0100

    separate gitignores

--- arduino-serial-tester/target/debug/build/io-kit-sys-6e2b32bac23f1033/build_script_build-6e2b32bac23f1033.d
+++
@@ -1,5 +0,0 @@
-/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/build/io-kit-sys-6e2b32bac23f1033/build_script_build-6e2b32bac23f1033: /Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/io-kit-sys-0.4.1/build.rs
-
-/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/build/io-kit-sys-6e2b32bac23f1033/build_script_build-6e2b32bac23f1033.d: /Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/io-kit-sys-0.4.1/build.rs
-
-/Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/io-kit-sys-0.4.1/build.rs:
--- arduino-serial-tester/target/debug/build/libc-e44df50a2858ad2a/build_script_build-e44df50a2858ad2a.d
+++
@@ -1,5 +0,0 @@
-/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/build/libc-e44df50a2858ad2a/build_script_build-e44df50a2858ad2a: /Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.183/build.rs
-
-/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/build/libc-e44df50a2858ad2a/build_script_build-e44df50a2858ad2a.d: /Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.183/build.rs
-
-/Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.183/build.rs:
