b3617b9eed1f8714252406b97058a52200ea483a
Jakub Kužel <jakub.kuzel7@gmail.com>
2026-03-21 12:24:08 +0100

    motor-test separate gitignore

--- motor-test/target/debug/deps/bitflags-00c8323c2a577a0a.d
+++
@@ -1,7 +0,0 @@
-/home/pb25/Dokumenty/GitHub/Feeder/motor-test/target/debug/deps/libbitflags-00c8323c2a577a0a.rmeta: /home/pb25/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs
-
-/home/pb25/Dokumenty/GitHub/Feeder/motor-test/target/debug/deps/libbitflags-00c8323c2a577a0a.rlib: /home/pb25/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs
-
-/home/pb25/Dokumenty/GitHub/Feeder/motor-test/target/debug/deps/bitflags-00c8323c2a577a0a.d: /home/pb25/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs
-
-/home/pb25/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs:
--- motor-test/target/debug/deps/motor_test-90e6299268f0b7b5.d
+++
@@ -1,5 +0,0 @@
-/home/pb25/Dokumenty/GitHub/Feeder/motor-test/target/debug/deps/motor_test-90e6299268f0b7b5: src/main.rs
-
-/home/pb25/Dokumenty/GitHub/Feeder/motor-test/target/debug/deps/motor_test-90e6299268f0b7b5.d: src/main.rs
-
-src/main.rs:
