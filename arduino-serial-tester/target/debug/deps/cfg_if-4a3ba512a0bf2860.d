b8e2f0b805566af002292594353c73f9175f2ea7
Jakub Kužel <jakub.kuzel7@gmail.com>
2026-03-21 12:20:04 +0100

    separate gitignores

--- arduino-serial-tester/target/debug/deps/cfg_if-4a3ba512a0bf2860.d
+++
@@ -1,5 +0,0 @@
-/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/deps/libcfg_if-4a3ba512a0bf2860.rmeta: /Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.4/src/lib.rs
-
-/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/deps/cfg_if-4a3ba512a0bf2860.d: /Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.4/src/lib.rs
-
-/Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.4/src/lib.rs:
--- arduino-serial-tester/target/debug/deps/scopeguard-17fb1f1a14b76f37.d
+++
@@ -1,5 +0,0 @@
-/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/deps/libscopeguard-17fb1f1a14b76f37.rmeta: /Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/scopeguard-1.2.0/src/lib.rs
-
-/Users/jakub/Documents/GitHub/Feeder/arduino-serial-tester/target/debug/deps/scopeguard-17fb1f1a14b76f37.d: /Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/scopeguard-1.2.0/src/lib.rs
-
-/Users/jakub/.cargo/registry/src/index.crates.io-6f17d22bba15001f/scopeguard-1.2.0/src/lib.rs:
