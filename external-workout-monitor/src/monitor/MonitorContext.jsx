ef195c812d425992b0cba24fb7cf905454fbc078
Jakub Kužel <jakub.kuzel7@gmail.com>
2026-04-08 16:52:48 +0200

    migration to separate git + small changes

--- external-workout-monitor/src/monitor/MonitorContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const MonitorContext = createContext(null);
-
-export function useMonitor() {
-  return useContext(MonitorContext);
-}
--- src/components/ManualScheduler.jsx
+++ src/components/ManualScheduler.jsx
@@ -12,12 +12,40 @@ export default function ManualSchedular({ formData, setFormData }) {
     <div className="flex flex-col items-center justify-center">
       <div className="flex items-center justify-between w-[500px]">
         <label className="font-medium text-nowrap text-gray-700 w-[110px]">
-          Interval střelby
+          Počet opakování
+        </label>
+        <input
+          type="range"
+          name="repetition"
+          min="1"
+          max="50"
+          value={formData.repetition}
+          onChange={(e) => updateFormData(e.target.name, e.target.value)}
+          className="w-64"
+        />
+        <input
+          type="number"
+          value={formData.repetition}
+          name="repetition"
+          readOnly
+          onFocus={(e) =>
+            showKeyboard(e, (newValue) =>
+              newValue > 0 && newValue <= 100
+                ? updateFormData(e.target.name, newValue)
+                : null,
+            )
+          }
+          className="w-16 border border-gray-300 rounded p-1"
+        />
+      </div>
+      <div className="flex items-center justify-between w-[500px]">
+        <label className="font-medium text-nowrap text-gray-700 w-[110px]">
+          Interval střelby (s)
         </label>
         <input
           type="range"
           name="interval"
-          min="1"
+          min="2"
           max="15"
           value={formData.interval || 5}
           onChange={(e) => updateFormData(e.target.name, e.target.value)}
@@ -32,35 +60,7 @@ export default function ManualSchedular({ formData, setFormData }) {
             showKeyboard(e, (newValue) =>
               newValue <= 20 && newValue >= 0
                 ? updateFormData(e.target.name, newValue)
-                : null
-            )
-          }
-          className="w-16 border border-gray-300 rounded p-1"
-        />
-      </div>
-      <div className="flex items-center justify-between w-[500px]">
-        <label className="font-medium text-nowrap text-gray-700 w-[110px]">
-          Počet opakování
-        </label>
-        <input
-          type="range"
-          name="repetition"
-          min="1"
-          max="100"
-          value={formData.repetition}
-          onChange={(e) => updateFormData(e.target.name, e.target.value)}
-          className="w-64"
-        />
-        <input
-          type="number"
-          value={formData.repetition}
-          name="repetition"
-          readOnly
-          onFocus={(e) =>
-            showKeyboard(e, (newValue) =>
-              newValue > 0 && newValue <= 100
-                ? updateFormData(e.target.name, newValue)
-                : null
+                : null,
             )
           }
           className="w-16 border border-gray-300 rounded p-1"
